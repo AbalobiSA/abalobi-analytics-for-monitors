829f1e46f1741191424dc4fc775b06630d5c70cc
Arno Van Der Merwe <arnovandermerwe39@gmail.com>
2016-07-13 10:05:30 +0200

    make it better

--- www/app/auth0.variables.js
+++ www/app/auth0.variables.js
@@ -1,3 +1,3 @@
-var AUTH0_CLIENT_ID='{OpQWLUNnYwukHVEUW6EIknk1qAIZLfJ0}';
+var AUTH0_CLIENT_ID='OpQWLUNnYwukHVEUW6EIknk1qAIZLfJ0';
 var AUTH0_CALLBACK_URL=location.href;
-var AUTH0_DOMAIN='{app53514901.eu.auth0.com}';
+var AUTH0_DOMAIN='app53514901.eu.auth0.com';
