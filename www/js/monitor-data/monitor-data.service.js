d2db0744cee97fce5736721e1ad6f1872627c89c
acawood777 <acawood777@gmail.com>
2016-09-19 11:21:33 +0200

    Undoing some changes

--- www/js/monitor-data/monitor-data.service.js
+++ www/js/monitor-data/monitor-data.service.js
@@ -1,8 +1,14 @@
 // These were moved to ENV var API_URL
-//var API_LIVE_URL = "https://abalobi-analytics-for-monitors.herokuapp.com/api/get/";
+var API_LIVE_URL = "https://abalobi-analytics-for-monitors.herokuapp.com/api/get/";
+//Eduardo:
 //var API_DEV_URL = "http://localhost:5001/api/get/";
+//Andrew:
+var API_DEV_URL = "http://localhost:5001/api/get/";
 
-var API_URL =  = process.env.API_URL;
+
+//var API_URL =  = process.env.API_URL;
+//var API_URL =  = API_LIVE_URL;
+var API_URL =  = API_LIVE_URL;
 
 angular.module('monitorData')
     .factory('MonitorResource', ['$resource',
