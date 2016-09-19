972965625417e9183d6930c8dcfd902439d03394
acawood777 <acawood777@gmail.com>
2016-09-19 11:05:36 +0200

    more

--- www/js/monitor-data/monitor-data.service.js
+++ www/js/monitor-data/monitor-data.service.js
@@ -1,8 +1,9 @@
-var API_LIVE_URL = "https://abalobi-analytics-for-monitors.herokuapp.com/api/get/";
-var API_DEV_URL = "http://localhost:5001/api/get/";
+// These were moved to ENV var API_URL
+//var API_LIVE_URL = "https://abalobi-analytics-for-monitors.herokuapp.com/api/get/";
+//var API_DEV_URL = "http://localhost:5001/api/get/";
 
-//TODO: Set this up to use env variables instead of needing to change this value for local testing
-var API_URL = API_LIVE_URL;
+var API_URL =  = process.env.API_URL;
+
 angular.module('monitorData')
     .factory('MonitorResource', ['$resource',
     function($resource) {
