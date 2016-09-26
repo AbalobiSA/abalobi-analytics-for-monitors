4f9e3266a96652672adf193351e6a85deec7093c
Eduardo Koloma Jr <eduardo@kolomajr.com>
2016-09-27 00:02:45 +0200

    fix: set api url to live

--- www/js/monitor-data/monitor-data.service.js
+++ www/js/monitor-data/monitor-data.service.js
@@ -5,7 +5,7 @@
     var API_DEV_URL = "http://localhost:5001/api/get/";
     var API_DEV_URL_ANDREW = "http://localhost:5000/api/get/";
 
-    var API_URL = API_DEV_URL;
+    var API_URL = API_LIVE_URL;
 
     angular.module('monitorData')
         .factory('MonitorResource', ['$resource',
