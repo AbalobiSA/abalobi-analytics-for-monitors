1e07786e647fe289df7448efb501967cf2076c14
Carl Eiserman <carleiserman@gmail.com>
2016-11-29 12:05:09 +0200

    Reworked app.js and app.run.js to be anonymous

--- www/js/app.js
+++ www/js/app.js
@@ -5,24 +5,53 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-var app = angular.module('app', ['ionic',
-        'monthlyCatchSpecies', 'monthlyBoatDistributionModule', 'monthlyCatchSpeciesByBoatTypeModule',
-        'submissionsByMonthLocationModule', 'catchSpeciesByLocationModule',
-        'sampleAveragesByLocationModule', 'utilsModule', 'monthlyCatchSpeciesByPermitTypeModule',
-        'app.controllers', 'app.directives', 'app.routes', 'app.services', 'auth0.lock', 'angular-jwt', 'ui.router']);
 
-app.run(function($ionicPlatform, authService, lock) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+
+// app.js
+
+(function () {
+
+  'use strict';
+
+  angular
+    .module('app', ['ionic',
+            'monthlyCatchSpecies', 'monthlyBoatDistributionModule', 'monthlyCatchSpeciesByBoatTypeModule',
+            'submissionsByMonthLocationModule', 'catchSpeciesByLocationModule',
+            'sampleAveragesByLocationModule', 'utilsModule', 'monthlyCatchSpeciesByPermitTypeModule',
+            'app.controllers', 'app.directives', 'app.routes', 'app.services', 'auth0.lock', 'angular-jwt', 'ui.router'])
+    .config(config);
+
+    function config(){
+      //Do nothing here
     }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-    authService.registerAuthenticationListener();
-  });
-})
+
+})();
+//
+//
+//
+//
+//
+//
+//
+//
+// var app = angular.module('app', ['ionic',
+//         'monthlyCatchSpecies', 'monthlyBoatDistributionModule', 'monthlyCatchSpeciesByBoatTypeModule',
+//         'submissionsByMonthLocationModule', 'catchSpeciesByLocationModule',
+//         'sampleAveragesByLocationModule', 'utilsModule', 'monthlyCatchSpeciesByPermitTypeModule',
+//         'app.controllers', 'app.directives', 'app.routes', 'app.services', 'auth0.lock', 'angular-jwt', 'ui.router']);
+
+// app.run(function($ionicPlatform, authService, lock) {
+//   $ionicPlatform.ready(function() {
+//     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+//     // for form inputs)
+//     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+//       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+//       cordova.plugins.Keyboard.disableScroll(true);
+//     }
+//     if (window.StatusBar) {
+//       // org.apache.cordova.statusbar required
+//       StatusBar.styleDefault();
+//     }
+//     authService.registerAuthenticationListener();
+//   });
+// })
